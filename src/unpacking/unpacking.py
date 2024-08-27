3e004229c4ee6c765d267982c05db3b7c8e46743
Chih-chan Tien <chihchan.tien@gmail.com>
2024-08-27 18:56:42 -0400

    fixed bug with packedpart

--- src/unpacking/unpacking.py
+++ src/unpacking/unpacking.py
@@ -27,12 +27,12 @@ def packed(fnct: Callable) -> Callable:
 
 @dispatch
 def apply_packed_filtered(fnct: Callable, squn: Sequence) -> Any:
-    return fnct(*squn[: len(signature(f).parameters)])
+    return fnct(*squn[: len(signature(fnct).parameters)])
 
 
 @dispatch
 def apply_packed_filtered(fnct: Callable, tbl_assc: Mapping) -> Any:
-    return fnct(**keyfilter(crr(contains)(signature(f).parameters.keys()), tbl_assc))
+    return fnct(**keyfilter(crr(contains)(signature(fnct).parameters.keys()), tbl_assc))
 
 
 def packedpart(fnct: Callable) -> Callable:
@@ -44,21 +44,21 @@ def packedpart(fnct: Callable) -> Callable:
 
 if __name__ == "__main__":
 
-    def f(x: int, y: int) -> int:
+    def test_fnct_add(x: int, y: int) -> int:
         added = x + y
         print(f"{x} added to {y} produces {added}")
         return added
 
     data_args = [1, 2]
     data_kwargs = {"x": 1, "y": 2}
-    assert apply_packed(f, data_args) == 3
-    assert packed(f)(data_args) == 3
-    assert apply_packed(f, data_kwargs) == 3
-    assert packed(f)(data_kwargs) == 3
+    assert apply_packed(test_fnct_add, data_args) == 3
+    assert packed(test_fnct_add)(data_args) == 3
+    assert apply_packed(test_fnct_add, data_kwargs) == 3
+    assert packed(test_fnct_add)(data_kwargs) == 3
 
     data_args_excess = [1, 2, 3]
     data_kwargs_excess = {"x": 1, "y": 2, "z": 3}
-    assert apply_packed_filtered(f, data_args_excess) == 3
-    assert packedpart(f)(data_args_excess) == 3
-    assert apply_packed_filtered(f, data_kwargs_excess) == 3
-    assert packedpart(f)(data_kwargs_excess) == 3
+    assert apply_packed_filtered(test_fnct_add, data_args_excess) == 3
+    assert packedpart(test_fnct_add)(data_args_excess) == 3
+    assert apply_packed_filtered(test_fnct_add, data_kwargs_excess) == 3
+    assert packedpart(test_fnct_add)(data_kwargs_excess) == 3
