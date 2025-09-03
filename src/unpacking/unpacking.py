3b1482512c8ffa095869db631a58db2b60099e3e
Chih-chan Tien <chihchan.tien@gmail.com>
2025-09-03 18:49:55 -0500

    organized

--- src/unpacking/unpacking.py
+++ src/unpacking/unpacking.py
@@ -11,21 +11,6 @@ from plum import dispatch, overload
 # the module uses plum's mypy integration (https://beartype.github.io/plum/integration.html)
 
 
-@overload
-def apply_packed[T](fnct: Callable[..., T], x: Iterable[Any]) -> T:
-    return fnct(*x)
-
-
-@overload
-def apply_packed[T](fnct: Callable[..., T], x: Mapping[str, Any]) -> T:
-    return fnct(**x)
-
-
-@dispatch
-def apply_packed[T](fnct: Callable[..., T], x: Iterable[Any] | Mapping[str, Any]) -> T:  # type: ignore[empty-body]
-    pass
-
-
 class starred[T]:
     def __init__(self, fnct: Callable[..., T]):
         self.fnct = fnct
@@ -42,27 +27,27 @@ class doublestarred[T]:
         return self.fnct(**x)
 
 
+@overload
+def apply_packed[T](fnct: Callable[..., T], x: Iterable[Any]) -> T:
+    return fnct(*x)
+
+
+@overload
+def apply_packed[T](fnct: Callable[..., T], x: Mapping[str, Any]) -> T:
+    return fnct(**x)
+
+
+@dispatch
+def apply_packed[T](fnct: Callable[..., T], x: Iterable[Any] | Mapping[str, Any]) -> T:  # type: ignore[empty-body]
+    pass
+
+
 class unpacking[T]:
     def __init__(self, fnct: Callable[..., T]):
         self.fnct = fnct
 
     def __call__(self, x: Iterable[Any] | Mapping[str, Any]) -> T:
         return apply_packed(self.fnct, x)
-
-
-@overload
-def apply_packed_part[T](fnct: Callable[..., T], x: Iterable[Any]) -> T:
-    return fnct(*islice(x, len(signature(fnct).parameters)))
-
-
-@overload
-def apply_packed_part[T](fnct: Callable[..., T], x: Mapping[str, Any]) -> T:
-    return fnct(**keyfilter(prt(contains, signature(fnct).parameters.keys()), x))
-
-
-@dispatch
-def apply_packed_part[T](fnct: Callable[..., T], x: Iterable[Any] | Mapping[str, Any]) -> T:  # type: ignore[empty-body]
-    pass
 
 
 class starredpart[T]:
@@ -79,6 +64,21 @@ class doublestarredpart[T]:
 
     def __call__(self, x: Mapping[str, Any]) -> T:
         return self.fnct(**keyfilter(prt(contains, signature(self.fnct).parameters.keys()), x))
+
+
+@overload
+def apply_packed_part[T](fnct: Callable[..., T], x: Iterable[Any]) -> T:
+    return fnct(*islice(x, len(signature(fnct).parameters)))
+
+
+@overload
+def apply_packed_part[T](fnct: Callable[..., T], x: Mapping[str, Any]) -> T:
+    return fnct(**keyfilter(prt(contains, signature(fnct).parameters.keys()), x))
+
+
+@dispatch
+def apply_packed_part[T](fnct: Callable[..., T], x: Iterable[Any] | Mapping[str, Any]) -> T:  # type: ignore[empty-body]
+    pass
 
 
 class unpackingpart[T]:
